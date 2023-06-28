acafc460ba1a33bb3f38ca13bb388e24153efb0e
Kimber Ogden <mx.kimber@gmail.com>
2023-06-27 20:18:17 -0500

    Pass plant data as props to PlantsIndex component and handle errors, render api data in plantsIndex, install Axios

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,31 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { PlantsIndex } from "./PlantsIndex";
 
 export function Content() {
+  const [plants, setPlants] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleIndexPhotos = () => {
+    axios.get("http://localhost:3000/plants.json")
+      .then((response) => {
+        console.log(response.data);
+        setPlants(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setErrorMessage('Failed to fetch plants. Please try again later.');
+      });
+  };
+
+  useEffect(() => {
+    handleIndexPhotos();
+  }, []);
+
   return (
     <div>
-      <h2><PlantsIndex /></h2>
+      {errorMessage && <p>{errorMessage}</p>}
+      <PlantsIndex plants={plants} />
     </div>
-  )
+  );
 }
--- src/PlantsIndex.jsx
+++ src/PlantsIndex.jsx
@@ -1,7 +1,17 @@
-export function PlantsIndex() {
+export function PlantsIndex(props) {
   return (
     <div>
       <h1>All Plants</h1>
+      {props.plants.map((plant) => (
+        <div key={plant.id}>
+          <h2>{plant.common_name}</h2>
+          <img src={plant.img} />
+          <p>Width: {plant.latin_name}</p>
+          <p>Watering: {plant.watering}</p>
+          <p>Ideal Light: {plant.light_ideal}</p>
+          <p>Height: {plant.category}</p>
+        </div>
+      ))}
     </div>
   );
 }

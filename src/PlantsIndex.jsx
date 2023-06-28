2fba36fa9a2a8db97b9605488b848dc1b9195d08
Kimber Ogden <mx.kimber@gmail.com>
2023-06-27 23:28:28 -0500

    Enable search feature for Plants index, temporarily limit shown results

--- src/PlantsIndex.jsx
+++ src/PlantsIndex.jsx
@@ -1,11 +1,31 @@
+import React, { useState } from 'react';
+
 export function PlantsIndex(props) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const { plants } = props;
+  const filteredPlants = plants
+  .filter(
+    (plant) =>
+      plant.common_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      plant.latin_name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+  .slice(0, 20); 
+
   return (
     <div>
       <h1>All Plants</h1>
-      {props.plants.map((plant) => (
+  
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+        placeholder="Search plants..."
+      />
+
+      {filteredPlants.map((plant) => (
         <div key={plant.id}>
           <h2>{plant.common_name}</h2>
-          <img src={plant.img} />
+          <img src={plant.img} alt={plant.common_name} />
           <p>Latin Name: {plant.latin_name}</p>
           <p>Watering: {plant.watering}</p>
           <p>Ideal Light: {plant.light_ideal}</p>
@@ -16,3 +36,4 @@ export function PlantsIndex(props) {
     </div>
   );
 }
+

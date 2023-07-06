38f1e8053210d82ee78789f92efc2ad4abf39b16
Kimber Ogden <mx.kimber@gmail.com>
2023-07-06 17:34:37 -0500

    Implement conditional rendering for search inputs and modify index pagination

--- src/PlantsIndex.jsx
+++ src/PlantsIndex.jsx
@@ -1,34 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export function PlantsIndex(props) {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
   const [currentPage, setCurrentPage] = useState(0);
-  const plantsPerPage = 3; 
+  const plantsPerPage = 6;
   const { plants } = props;
-  const filteredPlants = plants.filter(
-    (plant) =>
-      plant.common_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      plant.latin_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      plant.category.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const categories = [
+    'Palm',
+    'Anthurium',
+    'Other',
+    'Aglaonema',
+    'Hanging',
+    'Bromeliad',
+    'Spathiphyllum',
+    'Flower',
+    'Aralia',
+    'Ficus',
+    'Sansevieria',
+    'Foliage plant',
+    'Dieffenbachia',
+    'Philodendron',
+    'Cactus & Succulent',
+    'Schefflera',
+    'Topiairy',
+    'Fern',
+    'Grass',
+    'Ground Cover',
+  ];
 
-   filteredPlants.sort((a, b) => a.category.localeCompare(b.category));
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [selectedCategory, searchTerm]);
 
   const startIndex = currentPage * plantsPerPage;
   const endIndex = startIndex + plantsPerPage;
+
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    const selectedCategory = event.target.value;
+    setSelectedCategory(selectedCategory);
+    setSearchTerm('');
+  };
+
+  const filteredPlants = plants.filter((plant) => {
+    const searchTermMatch =
+      searchTerm === '' ||
+      plant.common_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      plant.latin_name.toLowerCase().includes(searchTerm.toLowerCase());
+
+    const categoryMatch =
+      selectedCategory === '' ||
+      plant.category.toLowerCase().includes(selectedCategory.toLowerCase());
+
+    return searchTermMatch && categoryMatch;
+  });
+
+  filteredPlants.sort((a, b) => a.category.localeCompare(b.category));
 
   const currentPlants = filteredPlants.slice(startIndex, endIndex);
 
   const handleNextPage = () => {
     setCurrentPage((prevPage) => {
-      window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to top of the page
+      window.scrollTo({ top: 0, behavior: 'smooth' });
       return prevPage + 1;
     });
   };
 
   const handlePreviousPage = () => {
     setCurrentPage((prevPage) => {
-      window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to top of the page
+      window.scrollTo({ top: 0, behavior: 'smooth' });
       return prevPage - 1;
     });
   };
@@ -36,13 +80,32 @@ export function PlantsIndex(props) {
   return (
     <div>
       <h1>All Plants</h1>
-  
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(event) => setSearchTerm(event.target.value)}
-        placeholder="Search plants..."
-      />
+
+      <div>
+        <label>
+          {selectedCategory ? 'Search within selected category:' : 'Search all plants:'}
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchTermChange}
+            placeholder={selectedCategory ? `Search within ${selectedCategory} category...` : 'Search plants...'}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Search By Category:
+          <select value={selectedCategory} onChange={handleCategoryChange}>
+            <option value="">All Categories</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
 
       {currentPlants.map((plant) => (
         <div key={plant.id}>

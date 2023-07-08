d8da883e3cc45480ed1a010e738c69c528e363ca
Kimber Ogden <mx.kimber@gmail.com>
2023-07-08 18:23:13 -0500

    Clean up indentation in Content

--- src/Content.jsx
+++ src/Content.jsx
@@ -103,29 +103,28 @@ export function Content() {
     setCurrentCollectedPlant(collected);
   };
 
-   const handleUpdateCollectedPlant = (id, params, successCallback) => {
-       console.log("handling edit collected plant -OK", params);
-       axios.patch(`http://localhost:3000/collected_plants/${id}.json`, params).then((response) => {
-         setCollectedPlants(
-           collectedPlants.map((collectedPlant) => {
-             if (collectedPlant.id === response.data.id) {
-               return response.data;
-             } else {
-               return collectedPlant;
-             }
-           })
-         );
-         successCallback();
-         handleClose();
-         
-       });
-     };
+  const handleUpdateCollectedPlant = (id, params, successCallback) => {
+    console.log("handling edit collected plant -OK", params);
+    axios.patch(`http://localhost:3000/collected_plants/${id}.json`, params).then((response) => {
+      setCollectedPlants(
+        collectedPlants.map((collectedPlant) => {
+          if (collectedPlant.id === response.data.id) {
+            return response.data;
+          } else {
+            return collectedPlant;
+          }
+        })
+      );
+      successCallback();
+      handleClose();
+    });
+  };
 
-     const handleEditCollectedPlant = (collected) => {
-      console.log("Showing collected plant edit - OK", collected);
-      setCurrentCollectedPlant(collected);
-      setIsCollectedPlantEditVisible(true);
-    };
+  const handleEditCollectedPlant = (collected) => {
+    console.log("Showing collected plant edit - OK", collected);
+    setCurrentCollectedPlant(collected);
+    setIsCollectedPlantEditVisible(true);
+  };
 
   const handleClose = () => {
     setIsPlantsShowVisible(false);

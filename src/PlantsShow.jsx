dac7d6a8689b4d3429f1fb88dc1267ba44888ed4
Kimber Ogden <mx.kimber@gmail.com>
2023-07-10 14:24:42 -0500

    Implement delayed rendering in PlantsShow component

--- src/PlantsShow.jsx
+++ src/PlantsShow.jsx
@@ -1,4 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
 export function PlantsShow(props) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const delay = 4000;
+
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1>Plant information</h1>
@@ -11,7 +31,6 @@ export function PlantsShow(props) {
       <p>Tolerated Light: {props.plant.light_tolerated}</p>
       <p>Climate: {props.plant.climate}</p>
       <p>Resource: <a href={props.plant.url}>{props.plant.url}</a></p>
-
     </div>
   );
 }

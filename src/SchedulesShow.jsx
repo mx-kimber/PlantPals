77e8590393260bd972fbdf2e3773d8f659b4f52b
Kimber Ogden <mx.kimber@gmail.com>
2023-07-11 01:49:32 -0500

    Refactored SchedulesShow component to handle missing data and added destructuring for props

--- src/SchedulesShow.jsx
+++ src/SchedulesShow.jsx
@@ -1,33 +1,45 @@
+import React from "react";
+
 export function SchedulesShow(props) {
+  const { schedule } = props;
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
-    props.onUpdateSchedule(props.schedule.id, params, () => event.target.reset());
+    props.onUpdateSchedule(schedule.id, params, () => event.target.reset());
   };
     
   const handleClick = () => {
-    props.onDestroySchedule(props.schedule);
+    props.onDestroySchedule(schedule);
   };
 
+  if (!schedule || !schedule.collected_plant) {
+    return null;
+  }
+
   return (
-   <div>
-    <h1>Schedules</h1>
-    <h2>{props.schedule.collected_plant.nickname}</h2>
-   
-    <form onSubmit={handleSubmit}>
-        <div> 
-          <p>{props.schedule.collected_plant.watering}</p>
-          Watering Start Date <input defaultValue={props.schedule.watering_start_date} name="name" type="text" />
+    <div>
+      <h1>Schedules</h1>
+      <h2>{schedule.collected_plant.nickname}</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <p>{schedule.collected_plant.watering}</p>
+          Watering Start Date{" "}
+          <input defaultValue={schedule.watering_start_date} name="name" type="text" />
         </div>
-        <div> 
-          Days to Water: <input defaultValue={props.schedule.days_to_water} name="days_to_water" type="text" />
+        <div>
+          Days to Water:{" "}
+          <input defaultValue={schedule.days_to_water} name="days_to_water" type="text" />
         </div>
-        <p><button type="submit">Update Schedule</button></p>
-        <p><button onClick={handleClick}>Destroy Schedule</button></p>
+        <p>
+          <button type="submit">Update Schedule</button>
+        </p>
+        <p>
+          <button onClick={handleClick}>Destroy Schedule</button>
+        </p>
       </form>
     </div>
   );
 }
-
     

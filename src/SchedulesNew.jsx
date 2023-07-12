cb497e684f370d597b388334d771ece4c3ae752a
Kimber Ogden <mx.kimber@gmail.com>
2023-07-12 18:11:17 -0500

    Refactor SchedulesNew component form submission and error handling

--- src/SchedulesNew.jsx
+++ src/SchedulesNew.jsx
@@ -3,28 +3,27 @@ export function SchedulesNew(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
-    props.onCreateSchedule(params, () => event.target.reset());
+    console.log('handleSubmit params:', params);
+    props.onCreateSchedule(params)
+    window.location.reload();
   };
 
   return (
     <div>
-      <h1>New Schedule</h1>
-      <form onSubmit={handleSubmit} >
-        <div>
-          user id: <input name="user_id" type="integer" />
-        </div>
-        <div>
-          collected plant id: <input name="collected_plant_id" type="text" />
-        </div>
-        <div>
-          days to water: <input name="days_to_water" type="text" />
-        </div>
-        <div>
-          Watering Start Date: <input name="watering_start_date" type="datetime-local" />
-        </div>
+  <h1>New Schedule</h1>
+  <form onSubmit={handleSubmit}>
+    <input type="hidden" name="user_id" value={props.collectedPlant.user_id} />
+    <input type="hidden" name="collected_plant_id" value={props.collectedPlant.id} />
 
-        <button type="submit">Create Schedule</button>
-      </form>
+    <div>
+      days to water: <input name="days_to_water" type="text" />
     </div>
+    <div>
+      Watering Start Date: <input name="watering_start_date" type="datetime-local" />
+    </div>
+
+    <button type="submit">Create Schedule</button>
+  </form>
+</div>
   );
 }

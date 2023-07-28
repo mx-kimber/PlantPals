a3930c5fbd2469fd5d8485e545653ae9a115f8fb
Kimber Ogden <mx.kimber@gmail.com>
2023-07-28 14:32:53 -0500

    Testing user state - Add user greeting in About component

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,15 @@
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
+
 export function About() {
+const { currentUser } = useContext(UserContext);
+  
   return (
     <div>
-      <p>Welcome to PlantPals!</p>
+      {currentUser ? `Welcome, ${currentUser.first_name}!` : 'Welcome, Guest!'}
     </div>
-  )
+  );
 }
+
+
+  

c9ea692bab31229adcea2a0a1700e2a6f9d01dbd
Kimber Ogden <mx.kimber@gmail.com>
2023-08-03 01:24:23 -0500

    Conditionally render header based on user authentication status

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom";
 import { LogoutLink } from "./LogoutLink";
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
 
 export function Header() {
+  const { currentUser } = useContext(UserContext);
+
+  if (!currentUser) {
+    return null;
+  }
+
   return (
     <header>
       <nav>
-        <Link to ="/plants">Browse Plants</Link>
-        <Link to ="/about">About</Link>
-        <Link to ="/collected_plants">Collection</Link>
-        <Link to ="/schedules/dashboard">Schedules Dashboard</Link>
-        {/* <Link to ="/login">Login</Link> */}
-        {/* <Link to ="/signup">Signup</Link> */}
+        <Link to="/plants">Browse Plants</Link>
+        <Link to="/about">About</Link>
+        <Link to="/collected_plants">Collection</Link>
+        <Link to="/schedules/dashboard">Schedules Dashboard</Link>
         <LogoutLink />
-      </nav>
-      
+      </nav>    
     </header>
-  )
+  );
 }

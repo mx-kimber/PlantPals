ee6233502b6cbec10770dc4c15adb3e2fe7909ef
Kimber Ogden <mx.kimber@gmail.com>
2023-08-07 18:43:51 -0500

    Comment out conditional rendering of schedule data in CollectePlantsIndex, Improved Plant Display and Edit Feature in SchedulesIndex

--- src/CollectedPlantsIndex.jsx
+++ src/CollectedPlantsIndex.jsx
@@ -4,30 +4,30 @@ import './Index.css';
 export function CollectedPlantsIndex({
   collectedPlants,
   onShowCollectedPlant,
-  onCreateSchedule,
+  // onCreateSchedule,
 }) {
   const handleImageClick = (collectedPlant) => {
     onShowCollectedPlant(collectedPlant);
   };
 
-  const renderSchedule = (collectedPlant) => {
-    if (collectedPlant.schedule?.watering_start_date) {
-      return (
-        <>
-          <p>Watering Schedule:</p>
-          <p>{collectedPlant.schedule.watering_start_date}</p>
-        </>
-      );
-    } else {
-      return (
-        <p>
-          <button onClick={() => onCreateSchedule(collectedPlant)}>
-            Create Schedule
-          </button>
-        </p>
-      );
-    }
-  };
+  // const renderSchedule = (collectedPlant) => {
+  //   if (collectedPlant.schedule?.watering_start_date) {
+  //     return (
+  //       <>
+  //         <p>Watering Schedule:</p>
+  //         <p>{collectedPlant.schedule.watering_start_date}</p>
+  //       </>
+  //     );
+  //   } else {
+  //     return (
+  //       <p>
+  //         <button onClick={() => onCreateSchedule(collectedPlant)}>
+  //           Create Schedule
+  //         </button>
+  //       </p>
+  //     );
+  //   }
+  // };
 
   const reversedCollectedPlants = [...collectedPlants].reverse();
 
@@ -44,7 +44,7 @@ export function CollectedPlantsIndex({
             alt="No image"
             onClick={() => handleImageClick(collectedPlant)}
           />
-          {renderSchedule(collectedPlant)}
+          {/* {renderSchedule(collectedPlant)} */}
         </div>
       ))}
     </div>

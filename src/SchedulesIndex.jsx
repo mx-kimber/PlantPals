37974571cc7d904c84c68de0b0a2137deb1f4499
Kimber Ogden <mx.kimber@gmail.com>
2023-08-07 18:44:19 -0500

    Improved Plant Display and Edit Feature in SchedulesIndex

--- src/SchedulesIndex.jsx
+++ src/SchedulesIndex.jsx
@@ -4,11 +4,32 @@ export function SchedulesIndex(props) {
       <h3>All Schedules</h3>
       {props.schedules.map((schedule) => (
         <div key={schedule.id}>
-          <h2>{schedule.collected_plant.nickname || schedule.collected_plant.common_name || schedule.collected_plant.latin_name}</h2>
+          <h2>
+            {schedule.collected_plant.nickname ||
+              schedule.collected_plant.common_name ||
+              schedule.collected_plant.latin_name}
+          </h2>
+          <p>
+            {schedule.collected_plant.custom_image ? (
+              <img
+                className="round-image"
+                src={schedule.collected_plant.custom_image}
+                alt="No image"
+              />
+            ) : (
+              <img
+                className="round-image"
+                src={schedule.collected_plant.img}
+                alt="No image"
+              />
+            )}
+          </p>
           <p>Days to Water: {schedule.days_to_water}</p>
           <p>Watering Start Date: {schedule.watering_start_date}</p>
-            <p>{schedule.collected_plant.watering}</p>
-            <button onClick={() => props.onShowSchedule(schedule)}>Edit Schedule</button>
+          <p>{schedule.collected_plant.watering}</p>
+          <button onClick={() => props.onShowSchedule(schedule)}>
+            Edit Schedule
+          </button>
         </div>
       ))}
     </div>

1fb3e5150ef75214362d23515e5e8e4250c18741
Kimber Ogden <mx.kimber@gmail.com>
2023-08-15 14:34:15 -0500

    Create Calendar.css file with a background color, import file into Calendar component, wrap FullCalendar in a container

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -4,34 +4,37 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from '@fullcalendar/list';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import './Calendar.css'; 
 
 class Calendar extends React.Component {
   render() {
     const { schedules } = this.props;
 
     return (
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
-        initialView="dayGridMonth"
-        headerToolbar={{
-          start: 'prev today next',
-          center: 'title',
-          end: 'dayGridMonth timeGridWeek timeGridDay',
-        }}
-        weekends={true}
-        editable={true}
-        events={schedules.map((schedule) => ({
-          id: schedule.id,
-          title: "Watering",
-          start: new Date(schedule.watering_start_date),
-          allDay: false,
-        }))}
-        dateClick={this.handleDateClick}
-        eventClick={(arg) => {
-          // note: handle showing schedule info (collectedPlant)
-          // open edit modal?
-        }}
-      />
+      <div className="calendar-container">
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
+          initialView="dayGridMonth"
+          headerToolbar={{
+            start: 'prev today next',
+            center: 'title',
+            end: 'dayGridMonth timeGridWeek timeGridDay',
+          }}
+          weekends={true}
+          editable={true}
+          events={schedules.map((schedule) => ({
+            id: schedule.id,
+            title: "Watering",
+            start: new Date(schedule.watering_start_date),
+            allDay: false,
+          }))}
+          dateClick={this.handleDateClick}
+          eventClick={(arg) => {
+            // note: handle showing schedule info (collectedPlant)
+            // open edit modal?
+          }}
+        />
+      </div>
     );
   }
 

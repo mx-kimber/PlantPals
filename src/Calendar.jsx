b7b85f632786be0caab14f0cce974d1a52ba42b3
Kimber Ogden <mx.kimber@gmail.com>
2023-08-15 14:15:35 -0500

    Enhance Calendar component to include timeGrid and list views, and use schedules prop for event data

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from "@fullcalendar/interaction"
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
-export default class Calendar extends React.Component {
-
+class Calendar extends React.Component {
   render() {
+    const { schedules } = this.props;
+
     return (
       <FullCalendar
-        plugins={[ dayGridPlugin, interactionPlugin ]}
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          start: 'prev today next',
+          center: 'title',
+          end: 'dayGridMonth timeGridWeek timeGridDay',
+        }}
+        weekends={true}
+        editable={true}
+        events={schedules.map((schedule) => ({
+          id: schedule.id,
+          title: "Watering",
+          start: new Date(schedule.watering_start_date),
+          allDay: false,
+        }))}
         dateClick={this.handleDateClick}
+        eventClick={(arg) => {
+          // note: handle showing schedule info (collectedPlant)
+          // open edit modal?
+        }}
       />
-    )
+    );
   }
 
   handleDateClick = (arg) => {
@@ -19,3 +40,5 @@ export default class Calendar extends React.Component {
   }
 
 }
+
+export default Calendar;
